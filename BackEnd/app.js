e8c133129770c5e1dee665cebcb644b5332beb3a
Aparna Suresh <aparnasuresh0002@gmail.com>
2022-11-22 18:13:50 +0530

    Update app.js

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -25,7 +25,7 @@ const path = require('path');
 app.use(express.static('./dist'));
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist//index.html'));});
+    res.sendFile(path.join(__dirname + '/dist/index.html'));});
 
 
 
